e489fb9258a8a6f604100c0f242fcff1333dcbb7
= <luismacedo2001@outlook.com>
2022-05-24 21:15:13 -0300

    almost done

---
+++ client.py
@@ -0,0 +1,82 @@
+import json
+import socket
+import json
+
+HOST = "127.0.0.1"
+PORT = 2222
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+
+while True:
+    dados = s.recv(1024)
+    dadosString = dados.decode("utf-8")
+    print(dadosString)
+    print("Comandos disponíveis: [1: Raiz], [2: Potência], [3:Seno], [4: Cosseno], [5: Tangente], [6: Fatorial], [SAIR]")
+    comando = str(input("Digite o comando a enviar: "))
+    while not comando.startswith(('1','2','3','4','5','6', "SAIR")):
+        msg = "Não é um comando válido"
+        print(msg)
+        comando = input("Digite o comando a enviar: ")
+    if comando.startswith("SAIR"):
+        s.sendall(comando.encode())
+        s.close
+        print("Conexão encerrada.")
+        break
+    elif comando.startswith('1'):
+        comando1 = comando
+        raiz = input("Digite o número desejado: ")
+        list = {
+            'comando1': comando1,
+            'raiz': raiz
+        }
+        msg = str.encode(json.dumps(list))
+        s.sendall(msg)
+    elif comando.startswith('2'):
+        comando1 = comando
+        base = input("Digite a base da potência: ")
+        expoente = input("Digite o expoente:")
+        
+        list = {
+            'comando1': comando1,
+            'base': base,
+            'expoente': expoente
+        }
+        msg = str.encode(json.dumps(list))
+        s.sendall(msg)
+    elif comando.startswith('3'):
+        comando1 = comando
+        seno = input("Digite o valor do seno que deseja encontrar: ")
+        list = {
+            'comando1': comando1,
+            'seno': seno
+        }
+        msg = str.encode(json.dumps(list))
+        s.sendall(msg)
+    elif comando.startswith('4'):
+        comando1 = comando
+        cosseno = input("Digite o valor do cosseno que deseja encontrar: ")
+        list = {
+            'comando1': comando1,
+            'cosseno': cosseno
+        }
+        msg = str.encode(json.dumps(list))
+        s.sendall(msg)
+    elif comando.startswith('5'):
+        comando1 = comando
+        tangente = input("Digite o valor da tangente que deseja encontrar: ")
+        list = {
+            'comando1': comando1,
+            'tangente': tangente
+        }
+        msg = str.encode(json.dumps(list))
+        s.sendall(msg)
+    else:
+        comando1 = comando
+        fatorial = input("Digite o valor do fatorial que deseja encontrar: ")
+        list = {
+            'comando1': comando1,
+            'fatorial': fatorial
+        }
+        msg = str.encode(json.dumps(list))
+        s.sendall(msg)
---
+++ server.py
@@ -0,0 +1,61 @@
+import socket
+import threading
+import json
+import math
+
+HOST = "127.0.0.1" # localhost
+PORT = 2222 # > 1023
+
+def atenderCliente(cliente, endereco):
+    print(f"Conectado em {endereco}")
+    while True:
+        
+        
+        menu = "Bem vindo a calculadora! Envie seu comando"
+        cliente.sendall(menu.encode())
+
+        dados = cliente.recv(1024).decode('utf-8')
+        msg = json.loads(dados)
+        print(f"{endereco} enviou o comando {msg}")
+        if msg['comando1'].startswith('1'):
+            print(f"raiz enviada: ", msg['raiz'])
+            resultado = math.sqrt(int(msg['raiz']))
+            mensagem = f"[INFO] Resultado: {resultado} \n"
+            cliente.sendall(mensagem.encode())
+        elif msg['comando1'].startswith('2'):
+            print(f"Base e expoente enviados: {msg['base']}, {msg['expoente']}")
+            resultado = int(msg['base'])**int(msg['expoente'])
+            mensagem = f"[INFO] Resultado da potencia: {resultado} \n"
+            cliente.sendall(mensagem.encode())
+        elif msg['comando1'].startswith('3'):
+            print(f"Seno enviado: {msg['seno']}")
+            resultado = math.sin(int(msg['seno']))
+            mensagem = f"[INFO] Resultado do seno: {resultado} \n"
+            cliente.sendall(mensagem.encode())
+        elif msg['comando1'].startswith('4'):
+            print(f"Cosseno enviado: {msg['cosseno']}")
+            resultado = math.cos(int(msg['cosseno']))
+            mensagem = f"[INFO] Resultado do cosseno: {resultado} \n"
+            cliente.sendall(mensagem.encode())
+        elif msg['comando1'].startswith('5'):
+            print(f"Tangente enviada: {msg['tangente']}")
+            resultado = math.tan(int(msg['tangente']))
+            mensagem = f"[INFO] Resultado da tangente: {resultado} \n"
+            cliente.sendall(mensagem.encode())
+        else:
+            print(f"Fatorial dada: {msg['fatorial']}")
+            resultado = math.factorial(int(msg['fatorial']))
+            mensagem = f"[INFO] Resultado do fatorial: {resultado} \n"
+            cliente.sendall(mensagem.encode())
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((HOST, PORT))
+s.listen(5)
+print("Aguardando conexões")
+while True:
+    cliente, endereco = s.accept()
+    cliente.settimeout(60)
+    threading.Thread(target=atenderCliente, args=(cliente, endereco)).start()
+
+
